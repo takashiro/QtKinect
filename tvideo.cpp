c5b71269a0b5a66a7ae1ff48cae9a4fe574bcfd8
Takashiro <takashiro@qq.com>
2015-01-29 20:00:32 +0800

    Add Video to display QImage frames

---
+++ tvideo.cpp
@@ -0,0 +1,33 @@
+#include "tvideo.h"
+
+#include <QPainter>
+#include <QTimer>
+
+#include <QDebug>
+
+TVideo::TVideo(QQuickItem *parent)
+    : QQuickPaintedItem(parent), m_frame("ui/image/default-background.jpg")
+{
+}
+
+void TVideo::setFrame(const QImage &image)
+{
+    m_frame = image;
+    update();
+}
+
+void TVideo::paint(QPainter *painter)
+{
+    QRect rect(0, 0, width(), height());
+    painter->drawImage(rect, m_frame);
+}
+
+class TVideoAdder
+{
+public:
+    TVideoAdder()
+    {
+        qmlRegisterType<TVideo>("Kinect", 1, 0, "TVideo");
+    }
+};
+TVideoAdder adder;
