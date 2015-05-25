bd89dced94baf9d4f895c0cfa0f330a3c899673e
Takashiro <takashiro@qq.com>
2015-05-25 12:23:45 +0800

    Add press recognition into TNuiHandArea

--- view/tnuihandarea.cpp
+++ view/tnuihandarea.cpp
@@ -7,24 +7,40 @@
 
 #include <QTimer>
 
+TNuiTracker *TNuiHandArea::m_leftTracker;
+TNuiTracker *TNuiHandArea::m_rightTracker;
+
 TNuiHandArea::TNuiHandArea(QQuickItem *parent)
     : QQuickItem(parent)
     , m_isUnderLeftHand(false)
     , m_isUnderRightHand(false)
+    , m_isPressed(false)
+    , m_pressDownRange(0.3f)
+    , m_pressUpRange(0.1f)
 {
     TNuiSensor *sensor = SensorManager->sensor();
 
-    static TNuiTracker *leftTracker = new TNuiTracker(sensor, NUI_SKELETON_POSITION_HAND_LEFT);
-    static TNuiTracker *rightTracker = new TNuiTracker(sensor, NUI_SKELETON_POSITION_HAND_RIGHT);
+    static TNuiTracker *leftTracker = m_leftTracker = new TNuiTracker(sensor, NUI_SKELETON_POSITION_HAND_LEFT);
     connect(leftTracker, &TNuiTracker::moved, this, &TNuiHandArea::onLeftHandMoved);
+
+    static TNuiTracker *rightTracker = m_rightTracker = new TNuiTracker(sensor, NUI_SKELETON_POSITION_HAND_RIGHT);
     connect(rightTracker, &TNuiTracker::moved, this, &TNuiHandArea::onRightHandMoved);
+
+    connect(this, &TNuiHandArea::exited, this, &TNuiHandArea::resetState);
 
     m_longTouchTimer = new QTimer(this);
     m_longTouchTimer->setSingleShot(true);
     m_longTouchTimer->setInterval(1500);
-    connect(m_longTouchTimer, &QTimer::timeout, this, &TNuiHandArea::checkLongTouch);
+    connect(m_longTouchTimer, &QTimer::timeout, this, &TNuiHandArea::longTouched);
     connect(this, &TNuiHandArea::entered, m_longTouchTimer, (void (QTimer::*)()) &QTimer::start);
     connect(this, &TNuiHandArea::exited, m_longTouchTimer, &QTimer::stop);
+
+    m_longPressTimer = new QTimer(this);
+    m_longPressTimer->setSingleShot(true);
+    m_longPressTimer->setInterval(1500);
+    connect(m_longPressTimer, &QTimer::timeout, this, &TNuiHandArea::longPressed);
+    connect(this, &TNuiHandArea::pressDown, m_longPressTimer, (void (QTimer::*)()) &QTimer::start);
+    connect(this, &TNuiHandArea::exited, m_longPressTimer, &QTimer::stop);
 }
 
 TNuiHandArea::~TNuiHandArea()
@@ -48,18 +64,41 @@ void TNuiHandArea::onHandMoved(bool &isUnderHand, const QPointF &pos)
     if (rect.contains(pos)) {
         if (!isUnderHand) {
             isUnderHand = true;
+            if (m_isUnderLeftHand) {
+                m_enterZ =  m_leftTracker->z();
+                connect(m_leftTracker, &TNuiTracker::zChanged, this, &TNuiHandArea::onHandZChanged);
+            } else {
+                m_enterZ =  m_rightTracker->z();
+                connect(m_rightTracker, &TNuiTracker::zChanged, this, &TNuiHandArea::onHandZChanged);
+            }
             emit entered();
         }
     } else {
         if (isUnderHand) {
+            disconnect(m_leftTracker, &TNuiTracker::zChanged, this, &TNuiHandArea::onHandZChanged);
+            disconnect(m_rightTracker, &TNuiTracker::zChanged, this, &TNuiHandArea::onHandZChanged);
             isUnderHand = false;
             emit exited();
         }
     }
 }
 
-void TNuiHandArea::checkLongTouch()
+void TNuiHandArea::onHandZChanged(float z)
 {
-    if (m_isUnderLeftHand || m_isUnderRightHand)
-        emit longTouched();
+    if (m_isPressed) {
+        if (m_enterZ - z < m_pressUpRange) {
+            m_isPressed = false;
+            emit pressUp();
+        }
+    } else {
+        if (m_enterZ - z > m_pressDownRange) {
+            m_isPressed = true;
+            emit pressDown();
+        }
+    }
+}
+
+void TNuiHandArea::resetState()
+{
+    m_isPressed = false;
 }
--- view/tnuihandarea.h
+++ view/tnuihandarea.h
@@ -3,34 +3,67 @@
 
 #include <QQuickItem>
 
+class TNuiTracker;
 class QTimer;
 
 class TNuiHandArea : public QQuickItem
 {
     Q_OBJECT
 
+    Q_PROPERTY(bool isUnderLeftHand READ isUnderLeftHand)
+    Q_PROPERTY(bool isUnderRightHand READ isUnderRightHand)
+    Q_PROPERTY(bool isPressed READ isPressed)
+    Q_PROPERTY(float pressDownRange READ pressDownRange WRITE setPressDownRange)
+    Q_PROPERTY(float pressUpRange READ pressUpRange WRITE setPressUpRange)
+
 public:
     TNuiHandArea(QQuickItem *parent = 0);
     ~TNuiHandArea();
 
-protected slots:
+    bool isUnderLeftHand() const { return m_isUnderLeftHand; }
+    bool isUnderRightHand() const { return m_isUnderRightHand; }
+    bool isPressed() const { return m_isPressed; }
+
+    float pressDownRange() const { return m_pressDownRange; }
+    void setPressDownRange(float range) { m_pressDownRange = range; }
+
+    float pressUpRange() const { return m_pressUpRange; }
+    void setPressUpRange(float range) { m_pressUpRange = range;}
+
+protected:
+    void resetState();
+
     void onLeftHandMoved(const QPointF &pos);
     void onRightHandMoved(const QPointF &pos);
     void onHandMoved(bool &isUnderHand, const QPointF &pos);
-
-    void checkLongTouch();
+    void onHandZChanged(float z);
 
 signals:
-    void entered();//Either hand enters
-    void exited(); //Either hand leaves
-
+    void entered();
+    void exited();
     void longTouched();//Either hand touches for over 1.5 seconds
+
+    void pressDown();
+    void pressUp();
+    void longPressed();//Either hand presses for over 1.5 seconds
+
+    void gripped();
+    void ungripped();
 
 private:
     bool m_isUnderLeftHand;
     bool m_isUnderRightHand;
+    bool m_isPressed;
 
     QTimer *m_longTouchTimer;
+    QTimer *m_longPressTimer;
+
+    float m_enterZ;
+    float m_pressDownRange;
+    float m_pressUpRange;
+
+    static TNuiTracker *m_leftTracker;
+    static TNuiTracker *m_rightTracker;
 };
 
 #endif // TNUIHANDAREA_H
