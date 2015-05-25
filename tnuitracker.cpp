afd4cfb03fd460eae7c47b7c1124ae4c8df9e68f
Takashiro <takashiro@qq.com>
2015-05-25 12:21:13 +0800

    Add z value of TNuiTracker

--- tnuitracker.cpp
+++ tnuitracker.cpp
@@ -23,13 +23,15 @@ QPointF MapToScreen(const Vector4 &point)
     return QPointF(x, y);
 }
 
+TNuiSkeletonStream *TNuiTracker::m_skeletonStream;
+
 TNuiTracker::TNuiTracker(TNuiSensor *sensor, NUI_SKELETON_POSITION_INDEX target)
     : QObject(sensor)
-    , m_skeletonStream(new TNuiSkeletonStream(sensor))
     , m_target(target)
 {
-    connect(m_skeletonStream, &TNuiSkeletonStream::readyRead, this, &TNuiTracker::handleNewFrame);
-    m_skeletonStream->tryOpen();
+    static TNuiSkeletonStream *stream = m_skeletonStream = new TNuiSkeletonStream(sensor);
+    connect(stream, &TNuiSkeletonStream::readyRead, this, &TNuiTracker::handleNewFrame);
+    stream->tryOpen();
 }
 
 void TNuiTracker::handleNewFrame()
@@ -43,8 +45,11 @@ void TNuiTracker::handleNewFrame()
     //@todo: Track more players
     for (int i = 0; i < NUI_SKELETON_COUNT; i++) {
         if (frame.SkeletonData[i].dwTrackingID != 0) {
-            QPointF pos = MapToScreen(frame.SkeletonData[i].SkeletonPositions[m_target]);
-            emit moved(pos);
+            const Vector4 &pos3d = frame.SkeletonData[i].SkeletonPositions[m_target];
+            m_screenPos = MapToScreen(pos3d);
+            emit moved(m_screenPos);
+            m_z = pos3d.z;
+            emit zChanged(m_z);
             break;
         }
     }
--- tnuitracker.h
+++ tnuitracker.h
@@ -2,6 +2,7 @@
 #define TNUITRACKER_H
 
 #include <QObject>
+#include <QPointF>
 
 #include <Windows.h>
 #include <NuiApi.h>
@@ -20,13 +21,20 @@ public:
     void setTarget(NUI_SKELETON_POSITION_INDEX target) { m_target = target; }
     NUI_SKELETON_POSITION_INDEX target() const { return m_target; }
 
+    QPointF screenPos() const { return m_screenPos; }
+    float z() const { return m_z; }
+
 signals:
     void moved(const QPointF &position);
+    void zChanged(float z);
 
 protected:
     void handleNewFrame();
 
-    TNuiSkeletonStream *m_skeletonStream;
+    QPointF m_screenPos;
+    float m_z;
+
+    static TNuiSkeletonStream *m_skeletonStream;
     NUI_SKELETON_POSITION_INDEX m_target;
 };
 
